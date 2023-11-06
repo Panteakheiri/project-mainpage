1615f3561a53332c5e6153b99722280b3d8dd383
pantea kheiri <irana.kheiri@yahoo.com>
2023-11-06 20:53:45 +0330

    arrows of sliders added

--- src/Components/Bestsellings/BestSellings.jsx
+++ src/Components/Bestsellings/BestSellings.jsx
@@ -1,54 +1,54 @@
-import React from 'react'
-import data from "../LastestProducts/data"
-import CardB from "./CardB"
-import arrowright from "../../assets/arrow1.png"
-import arrowleft from "../../assets/arrow2.png"
+import React from "react";
+import data from "../LastestProducts/data";
+import CardB from "./CardB";
+import arrow1 from "../../assets/arrow1.png";
+import arrow2 from "../../assets/arrow2.png";
 
 // core version + navigation, pagination modules:
-import {Swiper , SwiperSlide} from 'swiper/react';
-import { Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
 import SwiperCore from "swiper/core";
 // import Swiper and modules styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-
+import "swiper/css";
+import "./BestSelling.css"
 
 export default function BestSellings() {
-
   SwiperCore.use([Navigation]);
   return (
-    <div className='w-full flex  items-center flex-col mt-32'>
-      <h1 className='text-4xl font-bold mr-[1120px]'>Bestsellings</h1>
-      <p className='mt-4 text-[20px] mr-[940px] text-gray-400  mb-10'>Lorem ipsum dolor sit amet consectetur .</p>
-      <div className='w-full max-w-[1340px] flex justify-center' >
-      <Swiper
-            slidesPerGroup={1}
-            slidesPerView={5}
-            loop={true}
-            grabCursor={true}
-            navigation={{prevEl: '.prev' , nextEl:'.next'}}
-            allowSlideNext={true}
-            allowSlidePrev={true}
-            breakpoints={{
-              856 : {
-                slidesPerView : 5
-              },
-              640 : {
-                slidesPerView : 3
-              },
-              0 : {
-                slidesPerView : 1
-              }
-            }}
-            >
-                {data.map((item) => (
-                   <SwiperSlide key={item.id} className='relative'>
-                    <CardB name={item.name} img={item.img}/>
-                   </SwiperSlide>
-                ))
-                }
-            </Swiper>
+    <div className="w-full flex  items-center flex-col mt-32">
+      <h1 className="text-4xl font-bold mr-[1120px]">Bestsellings</h1>
+      <p className="mt-4 text-[20px] mr-[940px] text-gray-400  mb-10">
+        Lorem ipsum dolor sit amet consectetur .
+      </p>
+      <div className="w-full max-w-[1340px] justify-center">
+        <Swiper
+          modules={[Navigation]}
+          slidesPerGroup={1}
+          slidesPerView={5}
+          grabCursor={true}
+          loop={true}
+          navigation={{
+            nextEl: ".arrow-swiper-button-next",
+            prevEl: ".arrow-swiper-button-prev",
+            disabledClass: "swiper-button-disabled",
+          }}
+          
+        >
+          {data.map((item) => (
+            <SwiperSlide key={item.id} className="relative">
+              <CardB name={item.name} img={item.img} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+         
+        <div className='flex justify-center items-center w-full max-w-[1320px] mx-auto'>
+       <div className='arrows-divs w-full flex justify-end'>
+       <div className='arrow-next'><img src={arrow1} alt='left' className='arrow-swiper-button-next'/></div>
+       <div className='arrow-prev'><img src={arrow2} alt='right' className='arrow-swiper-button-prev'/></div>
+       </div>
+       </div>
+        
       </div>
     </div>
-  )
+  );
 }
--- src/Components/SummerOff/SliderCarpet.jsx
+++ src/Components/SummerOff/SliderCarpet.jsx
@@ -36,8 +36,13 @@ export default () => {
       <SwiperSlide><SummerOff/></SwiperSlide>
       <SwiperSlide><SummerOff/></SwiperSlide>
     </Swiper>
+    <div className='flex justify-center items-center w-full max-w-[1320px] mx-auto'>
+    <div className='arrows-div w-full flex justify-end mt-[-80px] mr-6'>
     <div className='swiper-next'><img src={left1} alt='left' className='image-swiper-button-next'/></div>
     <div className='swiper-prev'><img src={right1} alt='right' className='image-swiper-button-prev'/></div>
+    </div>
+    </div>
+    
     </>
   );
 };
